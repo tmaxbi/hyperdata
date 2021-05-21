f4cbe690229ee12935f4fc03d6ca84aa96cb7e4d
akakakakakaa <akstn3023@naver.com>
2021-05-21 15:50:04 +0900

    docker_load_and_push.sh 주석처리한 부분 수정

--- install/hypercloud/installer/docker_load_and_push.sh
+++ install/hypercloud/installer/docker_load_and_push.sh
@@ -32,18 +32,18 @@ function load_and_push_image {
   fi
 }
 
-# load_and_push_image images/installer_${INSTALLER_TAG}.tar hyperdata_installer ${INSTALLER_TAG}
-# load_and_push_image images/tb_${TB_TAG}.tar hyperdata8.3_tb ${TB_TAG}
-# load_and_push_image images/nginx_controller_${NGINX_CONTROLLER_TAG}.tar ingress-nginx/controller ${NGINX_CONTROLLER_TAG}
-# load_and_push_image images/nginx_certgen_${NGINX_CERTGEN_TAG}.tar jettech/kube-webhook-certgen ${NGINX_CERTGEN_TAG}
-# load_and_push_image images/hyperdata_${HD_TAG}.tar hyperdata8.3_hd_v8.3.4hotpatch ${HD_TAG}
-# load_and_push_image images/automl_${AUTOML_TAG}.tar hyperdata8.3_automl ${AUTOML_TAG}
-# load_and_push_image images/automl_doloader_${DOLOADER_TAG}.tar hyperdata8.3_automl_doloader ${DOLOADER_TAG}
-# load_and_push_image images/automl_fe_${FE_TAG}.tar hyperdata8.3_automl_fe ${FE_TAG}
-# load_and_push_image images/automl_xgb_${XGB_TAG}.tar hyperdata8.3_automl_hpb_xgb ${XGB_TAG}
-# load_and_push_image images/automl_rf_${RF_TAG}.tar hyperdata8.3_automl_hpb_rf ${RF_TAG}
-# load_and_push_image images/automl_downloader_${DOWNLOADER_TAG}.tar hyperdata8.3_automl_downloader ${DOWNLOADER_TAG}
-# load_and_push_image images/automl_scheduler_${SCHEDULER_TAG}.tar hyperdata8.3_scheduler ${SCHEDULER_TAG}
+load_and_push_image images/installer_${INSTALLER_TAG}.tar hyperdata_installer ${INSTALLER_TAG}
+load_and_push_image images/tb_${TB_TAG}.tar hyperdata8.3_tb ${TB_TAG}
+load_and_push_image images/nginx_controller_${NGINX_CONTROLLER_TAG}.tar ingress-nginx/controller ${NGINX_CONTROLLER_TAG}
+load_and_push_image images/nginx_certgen_${NGINX_CERTGEN_TAG}.tar jettech/kube-webhook-certgen ${NGINX_CERTGEN_TAG}
+load_and_push_image images/hyperdata_${HD_TAG}.tar hyperdata8.3_hd_v8.3.4hotpatch ${HD_TAG}
+load_and_push_image images/automl_${AUTOML_TAG}.tar hyperdata8.3_automl ${AUTOML_TAG}
+load_and_push_image images/automl_doloader_${DOLOADER_TAG}.tar hyperdata8.3_automl_doloader ${DOLOADER_TAG}
+load_and_push_image images/automl_fe_${FE_TAG}.tar hyperdata8.3_automl_fe ${FE_TAG}
+load_and_push_image images/automl_xgb_${XGB_TAG}.tar hyperdata8.3_automl_hpb_xgb ${XGB_TAG}
+load_and_push_image images/automl_rf_${RF_TAG}.tar hyperdata8.3_automl_hpb_rf ${RF_TAG}
+load_and_push_image images/automl_downloader_${DOWNLOADER_TAG}.tar hyperdata8.3_automl_downloader ${DOWNLOADER_TAG}
+load_and_push_image images/automl_scheduler_${SCHEDULER_TAG}.tar hyperdata8.3_scheduler ${SCHEDULER_TAG}
 load_and_push_image images/automl_woori_${WOORI_TAG}.tar hyperdata8.3_predefinedai_woori_text_classification_train ${WOORI_TAG}
 load_and_push_image images/automl_resultuploader_${RESULTUPLOADER_TAG}.tar hyperdata8.3_predefinedai_woori_text_classification_resultuploader ${RESULTUPLOADER_TAG}
 load_and_push_image images/automl_domainserving_${DOMAINSERVING_TAG}.tar hyperdata8.3_predefinedai_text_classification_serving ${DOMAINSERVING_TAG}
