cd278889ed559d51e1fb4f22ea0aea33d99615a6
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-02-21 22:16:32 +0900

    Update boot.sh

--- build/hyperdata/src/boot.sh
+++ build/hyperdata/src/boot.sh
@@ -4,7 +4,9 @@ set +e
 ## TODO: install_interactive.sh를 매번 돌리지 않아도 정상적으로 수행되도록 수정
 
 # set envs
-. $DEPLOY_HOME/set_envs.sh
+cp $DEPLOY_HOME/set_envs.sh ~/.bashrc
+source ~/.bashrc
+#. $DEPLOY_HOME/set_envs.sh
 
 # 1. boot jeus
 # jeus에서 set +e를 설정해서 터지면, boot.sh에 set -e 설정되있을 시 터져버림
