abf6777671b0adfb6819806ac0637788e3be565c
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-02-22 20:04:18 +0900

    Update init_container.sh

--- build/hyperdata/src/init_container.sh
+++ build/hyperdata/src/init_container.sh
@@ -4,39 +4,36 @@ set -e
 
 ## TODO: install_interactive.sh를 매번 돌리지 않아도 정상적으로 수행되도록 수정
 
-while [ ! -f $TB_MOUNT_VOLUME_PATH/tibero6/bin/tbprobe ]
-do
-  # set envs
-  . $DEPLOY_HOME/set_envs.sh
 
-  # 1. check until tibero init
-  bash $DEPLOY_HOME/init/wait_tibero.sh
+# set envs
+. $DEPLOY_HOME/set_envs.sh
 
-  # 2. init hyperdata
-  if [ ! -f $TB_MOUNT_VOLUME_PATH/HD_SCHEMA_VERSION ]; then
-    # if no hyperdata version exists
-    echo "Cannot find hyperdata schema version file. regard uninitialized."
-  
-    ## init hadoop
-    bash $DEPLOY_HOME/init/hadoop.sh
-  
-    ## init jeus
-    bash $DEPLOY_HOME/init/jeus.sh
-    
-    ## init hyperdata
-    bash $DEPLOY_HOME/init/hyperdata.sh
-  
-    ## save schema version in Tibero PVC
-    echo $HD_SCHEMA_VERSION > $TB_MOUNT_VOLUME_PATH/HD_SCHEMA_VERSION
-  
-    ## stop proauth
-    stopServer -host localhost:29736 -u jeus -p jeus
-  
-    ## stop hyperdata
-    stopServer -host localhost:19736 -u jeus -p jeus
+# 1. check until tibero init
+bash $DEPLOY_HOME/init/wait_tibero.sh
 
-    ## stop jeus
-    stopServer -host localhost:9736 -u jeus -p jeus
+# 2. init hyperdata
+if [ ! -f $TB_MOUNT_VOLUME_PATH/HD_SCHEMA_VERSION ]; then
+  # if no hyperdata version exists
+  echo "Cannot find hyperdata schema version file. regard uninitialized."
+  
+  ## init hadoop
+  bash $DEPLOY_HOME/init/hadoop.sh
+  
+  ## init jeus
+  bash $DEPLOY_HOME/init/jeus.sh
+   
+  ## init hyperdata
+  bash $DEPLOY_HOME/init/hyperdata.sh
+  
+  ## save schema version in Tibero PVC
+  echo $HD_SCHEMA_VERSION > $TB_MOUNT_VOLUME_PATH/HD_SCHEMA_VERSION
+  
+  ## stop proauth
+  stopServer -host localhost:29736 -u jeus -p jeus
+  
+  ## stop hyperdata
+  stopServer -host localhost:19736 -u jeus -p jeus
+
+  ## stop jeus
+  stopServer -host localhost:9736 -u jeus -p jeus
   fi
-  break
-done
