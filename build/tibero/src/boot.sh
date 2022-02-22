5e1d8ce091bed7b8e7641a1c6267ba096132f222
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-02-23 00:23:56 +0900

    Update boot.sh

--- build/tibero/src/boot.sh
+++ build/tibero/src/boot.sh
@@ -1,33 +1,18 @@
-#service ssh restart
-#mv /bin/sh /bin/sh.bak
-#ln -s bash /bin/sh
-#mkdir -p /db/client
-#if [ -d $JAVA_HOME ]; then echo "Java already exists."; else sh /deploy_src/src/java/install.sh; fi
-
-#useradd -d /db -s /bin/bash hyperdata
-#echo "hyperdata:hyperdata1234!" | chpasswd
-
-#chown -R hyperdata:hyperdata /db
-#chown -R hyperdata:hyperdata /deploy_src
+#!/bin/sh
 
 if [ -d $HADOOP_HOME ]; then 
   echo "Hadoop already exists."
-  cp /deploy_src/src/hadoop/hadoop_classpath2.conf ~/.bashrc
-
+  cp /deploy_src/src/hadoop/hadoop_classpath.conf ~/.bashrc
   echo "alias tlog='tail -f /db/tibero6/instance/tibero/log/slog/sys.log'" >> ~/.bashrc
-
   source ~/.bashrc
-  #. /deploy_src/src/hadoop/hadoop_classpath.conf
 else 
   sh /deploy_src/src/hadoop/install.sh 
 fi
 
 if [ $PV_RECOVERY = 'Y' ]; then 
   echo "Start PV Recovery and tbboot"
-
 else
   tar -xzf /deploy_src/src/tibero/*.tar.gz -C /db/
-
   if [ -d $TB_HOME/database ]; then 
     echo "Tibero already exists."
     sh /deploy_src/src/tibero/sync_hostip.sh
