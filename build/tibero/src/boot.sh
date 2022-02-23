fc0dae31e60cce3127dc4b606c764db097948c35
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-02-23 19:15:25 +0900

    Update boot.sh

--- build/tibero/src/boot.sh
+++ build/tibero/src/boot.sh
@@ -5,7 +5,7 @@ echo "alias tlog='tail -f /db/tibero6/instance/tibero/log/slog/sys.log'" >> ~/.b
 . ~/.bashrc
 
 if [ -d $HADOOP_HOME ]; then 
-  echo "Hadoop already exists.";
+  echo "Hadoop already exists."
 else 
   sh /deploy_src/src/hadoop/install.sh;
 fi
@@ -13,21 +13,19 @@ fi
 tar -xzf /deploy_src/src/tibero/*.tar.gz -C /db/
 
 if [ -d $TB_HOME/database ]; then
-  echo "Tibero already exists.";
+  echo "Tibero already exists."
   sh /deploy_src/src/tibero/sync_hostip.sh;
 else
   sh /deploy_src/src/tibero/install.sh;
 fi
 
 tbboot;
-tbdown immediate
-. ~/.bashrc
-tbboot
+tbdown immediate;
+tbboot;
 fi
 
 chmod 750 $TB_HOME/instance/$TB_SID/log
 find $TB_HOME/instance/$TB_SID/log -type f -name '*.log' -exec chmod 640 {} \;
-#chmod -R 640 $TB_HOME/database/$TB_SID/log/*.log
 chmod -R 640 $TB_HOME/database/$TB_SID/*.ctl
 chmod -R 640 $TB_HOME/database/$TB_SID/*.log
 chmod -R 640 $TB_HOME/database/$TB_SID/*.dtf
