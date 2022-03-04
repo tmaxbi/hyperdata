b45d92edc4fc80d1380affa06d5ea29a5abd2c5a
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-03-04 21:54:54 +0900

    Update boot.sh

--- build/hyperdata/src/boot.sh
+++ build/hyperdata/src/boot.sh
@@ -3,16 +3,12 @@ set +e
 
 ## TODO: install_interactive.sh를 매번 돌리지 않아도 정상적으로 수행되도록 수정
 
-# set envs
-cp $HOME/set_envs.sh ~/.bashrc
-. ~/.bashrc
-
 # 1. boot jeus
 # jeus에서 set +e를 설정해서 터지면, boot.sh에 set -e 설정되있을 시 터져버림
-bash $HOME/boot/jeus.sh
+bash $SRC_HOME/boot/jeus.sh
 
 # 2. boot hyperdata
-bash $HOME/boot/hyperdata.sh
+bash $SRC_HOME/boot/hyperdata.sh
 
 # if all boot process working, do inifinity loop. it makes the pod keep running.
 while true; do sleep 30; done; 
