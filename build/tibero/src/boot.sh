178a8da83bf94e19629cb1b2c0b07af557f087dd
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-03-04 11:42:41 +0900

    Update boot.sh

--- build/tibero/src/boot.sh
+++ build/tibero/src/boot.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-cp /deploy_src/src/hadoop/hadoop_classpath.conf ~/.bashrc
+cp /db/src/hadoop/hadoop_classpath.conf ~/.bashrc
 echo "alias tlog='tail -f /db/tibero6/instance/tibero/log/slog/sys.log'" >> ~/.bashrc
 . ~/.bashrc
 
 if [ -d $HADOOP_HOME ]; then 
   echo "Hadoop already exists."
 else 
-  sh /deploy_src/src/hadoop/install.sh;
+  sh /db/src/hadoop/install.sh;
 fi
 
-tar -xzf /deploy_src/src/tibero/*.tar.gz -C /db/
+tar -xzf /db/src/tibero/*.tar.gz -C /db/
 
 if [ -d $TB_HOME/database ]; then
   echo "Tibero already exists."
-  sh /deploy_src/src/tibero/sync_hostip.sh;
+  sh /db/src/tibero/sync_hostip.sh;
 else
-  sh /deploy_src/src/tibero/install.sh;
+  sh /db/src/tibero/install.sh;
 fi
 
 tbboot;
