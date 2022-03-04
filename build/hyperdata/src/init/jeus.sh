aeecb03da05479fcca2b91fd9c3dbc94974e4638
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-03-04 12:17:45 +0900

    Update jeus.sh

--- build/hyperdata/src/init/jeus.sh
+++ build/hyperdata/src/init/jeus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-. $DEPLOY_HOME/set_envs.sh
+. $HOME/set_envs.sh
 
 mkdir -p $JEUS_HOME
-cp -rp $DEPLOY_HOME/jeus8/* $JEUS_HOME
+cp -rp $HOME/jeus8/* $JEUS_HOME
 
 chmod -R 755 $JEUS_HOME/lib/etc/ant/bin
 cd $JEUS_HOME/setup
