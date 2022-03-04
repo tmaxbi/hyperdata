efe2ced5861b5a48df6186b9f02cc684c98ac65f
SebinKum <82432145+SebinKum@users.noreply.github.com>
2022-03-04 12:17:04 +0900

    Update hadoop.sh

--- build/hyperdata/src/init/hadoop.sh
+++ build/hyperdata/src/init/hadoop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-. $DEPLOY_HOME/set_envs.sh
+. $HOME/set_envs.sh
 mkdir -p $TB_VOLUME/krb5_conf
-cp $DEPLOY_HOME/hadoop/krb5.conf $TB_VOLUME/krb5_conf
+cp $HOME/hadoop/krb5.conf $TB_VOLUME/krb5_conf
