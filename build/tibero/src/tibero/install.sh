1df18c99247192225440afb92a56a067a6655ae5
BIQA1 <113074584+BIQA1@users.noreply.github.com>
2023-02-06 13:36:49 +0900

    [Bug fix] Tibero install.sh 내에 tibero.tip를 수정하는 스크립트가 작동하지 않아 regex 수정

--- build/tibero/src/tibero/install.sh
+++ build/tibero/src/tibero/install.sh
@@ -23,49 +23,49 @@ if [ $TB_PORT ]; then
 fi
 
 if [ $MAX_SESSION_COUNT ]; then 
-     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/MAX_SESSION_COUNT=20/MAX_SESSION_COUNT=$MAX_SESSION_COUNT/g" {} \;
+     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/MAX_SESSION_COUNT=.*/MAX_SESSION_COUNT=$MAX_SESSION_COUNT/g" {} \;
 fi
 
 if [ $TOTAL_SHM_SIZE ]; then 
-     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/TOTAL_SHM_SIZE=2/TOTAL_SHM_SIZE=$TOTAL_SHM_SIZE/g" {} \;
+     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/TOTAL_SHM_SIZE=.*/TOTAL_SHM_SIZE=$TOTAL_SHM_SIZE/g" {} \;
 fi
 
 if [ $MEMORY_TARGET ]; then 
-     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/MEMORY_TARGET=4/MEMORY_TARGET=$MEMORY_TARGET/g" {} \;
+     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/MEMORY_TARGET=.*/MEMORY_TARGET=$MEMORY_TARGET/g" {} \;
 fi
 
 if [ $LOG_BUFFER ]; then 
-     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/LOG_BUFFER=200M/LOG_BUFFER=$LOG_BUFFER/g" {} \;
+     find $TB_HOME/config/ -name "*.tip" -exec sed -i "s/LOG_BUFFER=.*/LOG_BUFFER=$LOG_BUFFER/g" {} \;
 fi
 
 if [ $LOG_GROUP ]; then 
-     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/log_group=3/log_group=$LOG_GROUP/g" {} \;
+     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/log_group=.*/log_group=$LOG_GROUP/g" {} \;
 fi
 
 if [ $LOG_FILE_SIZE ]; then 
-     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/log_size=50M/log_size=$LOG_FILE_SIZE/g" {} \;
+     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/log_size=.*/log_size=$LOG_FILE_SIZE/g" {} \;
 fi
 
 if [ $SYSTEM_FILE_SIZE ]; then 
-     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/system_size=100M/system_size=$SYSTEM_FILE_SIZE/g" {} \;
+     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/system_size=.*/system_size=$SYSTEM_FILE_SIZE/g" {} \;
 fi
 
 if [ $TEMP_FILE_SIZE ]; then 
-     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/temp_size=100M/temp_size=$TEMP_FILE_SIZE/g" {} \;
+     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/temp_size=.*/temp_size=$TEMP_FILE_SIZE/g" {} \;
 fi
 
 if [ $UNDO_FILE_SIZE ]; then 
-     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/undo_size=200M/undo_size=$UNDO_FILE_SIZE/g" {} \;
+     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/undo_size=.*/undo_size=$UNDO_FILE_SIZE/g" {} \;
 fi
 
 if [ $USR_FILE_SIZE ]; then 
-     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/usr_size=100M/usr_size=$USR_FILE_SIZE/g" {} \;
+     find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/usr_size=.*/usr_size=$USR_FILE_SIZE/g" {} \;
 fi
 
-find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/system_next_size=10M/system_next_size=100M/g" {} \;
-find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/temp_next_size=10M/temp_next_size=100M/g" {} \;
-find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/undo_next_size=10M/undo_next_size=100M/g" {} \;
-find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/usr_next_size=10M/usr_next_size=100M/g" {} \;
+find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/system_next_size=.*/system_next_size=100M/g" {} \;
+find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/temp_next_size=.*/temp_next_size=100M/g" {} \;
+find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/undo_next_size=.*/undo_next_size=100M/g" {} \;
+find $TB_HOME/bin/ -name "tbctl" -exec sed -i "s/usr_next_size=.*/usr_next_size=100M/g" {} \;
 
 if [ $DB_CHARACTER_SET ]; then 
      sh $TB_HOME/bin/tb_create_db.sh -ch $DB_CHARACTER_SET
