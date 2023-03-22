ccc7191475328b1aca48603c8d40764a98293b43
root <root@kubemaster1.cluster.local>
2023-03-22 16:37:58 +0900

    install.sh 작성

---
+++ install/onprem/system_management/keycloak/keycloak-script/configure-keycloak.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+export
+
+# **************** Global variables
+
+# **********************************************************************************
+# Functions definition
+# **********************************************************************************
+
+function configureKeycloak() {
+    echo "************************************"
+    echo " Configure Keycloak realm"
+    echo "************************************"
+
+    # Set the needed parameter
+    USER=admin
+    PASSWORD=admin
+    GRANT_TYPE=password
+    CLIENT_ID=admin-cli
+    CREATE_REALM=keycloak/keycloak-script/createRealm.json
+    echo $CREATE_REALM
+    KEYCLOAK_URL=$1
+    echo $KEYCLOAK_URL
+    access_token=$( curl -d "client_id=$CLIENT_ID" -d "username=$USER" -d "password=$PASSWORD" -d "grant_type=$GRANT_TYPE" "$KEYCLOAK_URL/realms/master/protocol/openid-connect/token" | sed -n 's|.*"access_token":"\([^"]*\)".*|\1|p')
+
+    echo "Access token : $access_token"
+
+    if [ "$access_token" = "" ]; then
+        echo "------------------------------------------------------------------------"
+        echo "Error:"
+        echo "======"
+        echo ""
+        echo "It seems there is a problem to get the Keycloak access token: ($access_token)"
+        echo "The script exits here!"
+        echo ""
+        echo "------------------------------------------------------------------------"
+        exit 1
+    fi
+
+    # Create the realm in Keycloak
+    echo "------------------------------------------------------------------------"
+    echo "Create the realm in Keycloak"
+    echo "------------------------------------------------------------------------"
+    echo ""
+
+    result=$(curl -d @"$CREATE_REALM" -H "Content-Type: application/json" -H "Authorization: bearer $access_token" "$KEYCLOAK_URL/admin/realms")
+
+    if [ "$result" = "" ]; then
+        echo "------------------------------------------------------------------------"
+        echo "The realm is created. "
+        echo "Open following link in your browser:"
+        echo "$KEYCLOAK_URL/admin/master/console/#/HyperDataRealm"
+        echo "------------------------------------------------------------------------"
+    else
+        echo "------------------------------------------------------------------------"
+        echo "Error:"
+        echo "======"
+        echo "It seems there is a problem with the realm creation: $result"
+        echo "The script exits here!"
+        echo ""
+        exit 1
+    fi
+}
+
+#**********************************************************************************
+# Execution
+# **********************************************************************************
+
+configureKeycloak $1
